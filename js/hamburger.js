6510ba91becf5db532f6b927f0eda0ddb7789af4
S-Odawara <34loves5@gmail.com>
2022-04-02 02:50:42 +0900

    mod css

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-    $("#nav-button").on("click", function(){
-        $(".sp-nav").slideToggle();
-    });
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#sp-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#sp-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#sp-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
