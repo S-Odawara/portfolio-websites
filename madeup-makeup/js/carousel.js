16962beeea1c87809fbf3e6ae7ad5b1b6f3961f0
S-Odawara <34loves5@gmail.com>
2022-04-10 13:53:21 +0900

    mod

--- madeup-makeup/js/carousel.js
+++ madeup-makeup/js/carousel.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
-    $('.carousel').slick({
+    $('.carousel-top').slick({
         autoplay: true, // 自動再生
-        autoplaySpeed: 3000, // 自動再生の速さ、単位はミリ秒
-        arrows: false,
+        autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+        arrows: true,
+        prevArrow:'<div class="prev">prev</div>',
+        nextArrow:'<div class="next">next</div>',
         dots: false, 
         fade: false, 
         centerMode: true,
-        centerPadding:'20%',
+        centerPadding:'0%',
         slidesToShow: 1,
+
+        cssEase: 'ease-out',
+    });
+});
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 3000, // 自動再生の速さ、単位はミリ秒
+        arrows: false,
+
+        dots: false, 
+        fade: true, 
+        centerMode: true,
+        centerPadding:'0%',
+        slidesToShow: 1,
+
+        cssEase: 'ease-out',
     });
 });
